bae7e166b0bdaeaa7e1a153c9bfcf5acef2be1f9
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-10-19 07:06:05 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import plotly.express as px
+import pandas as pd
 import numpy as np
 
 # Streamlitアプリケーションのタイトルを設定
@@ -8,9 +8,6 @@ st.title('y = x^n の3Dプロット')
 
 # 3Dプロットを作成する関数
 def plot_3d_graph(n):
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111, projection='3d')
-
     # x軸の範囲を設定（-10から10まで）
     x = np.linspace(-10, 10, 400)
 
@@ -18,32 +15,21 @@ def plot_3d_graph(n):
     y_positive = x**n
     y_negative = -x**n
 
-    # y > 0 の領域をプロット
-    ax.plot(x, y_positive, zs=0, zdir='y', label=f'y = x^{n} (y > 0)', color='b')
+    # データフレームを作成
+    data = np.vstack((x, y_positive)).T
+    data_negative = np.vstack((x, y_negative)).T
+    df_positive = pd.DataFrame(data, columns=['X', 'Y'])
+    df_negative = pd.DataFrame(data_negative, columns=['X', 'Y'])
 
-    # y < 0 の領域をプロット
-    ax.plot(x, y_negative, zs=0, zdir='y', label=f'y = -x^{n} (y < 0)', color='r')
+    # Plotlyの3Dプロットを作成
+    fig = px.line_3d(df_positive, x='X', y='Y', z=np.zeros_like(df_positive['X']), line_shape='linear', color_discrete_sequence=['blue'])
+    fig.add_scatter3d(x=df_negative['X'], y=df_negative['Y'], z=np.zeros_like(df_negative['X']), mode='lines', line=dict(color='red'))
 
-    # グラフを90°傾ける
-    ax.view_init(elev=20, azim=-90)
-
-    # x, y, z軸のラベルを設定
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-
-    # グラフを表示
-    plt.legend()
-    plt.tight_layout()
-    st.pyplot(fig)
+    # プロットを表示
+    st.plotly_chart(fig)
 
 # アプリケーションを実行する部分
 if __name__ == '__main__':
     # nの値をユーザーに入力させる
     n = st.slider('nの値を選択してください', min_value=1, max_value=10, value=2)
     plot_3d_graph(n)
-
-
-
-
-
