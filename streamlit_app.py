e08a2d05287a6bee59cab4fdc5b9315a5a10b48f
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-10-19 06:45:08 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,28 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 # Streamlitアプリケーションのタイトルを設定
-st.title('y = x^2 の3Dプロット')
+st.title('y = x^n の3Dプロット')
 
 # 3Dプロットを作成する関数
-def plot_3d_graph():
+def plot_3d_graph(n):
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
 
     # x軸の範囲を設定（-10から10まで）
     x = np.linspace(-10, 10, 400)
 
-    # y = x^2 の計算
-    y_positive = x**2
-    y_negative = -x**2
+    # y = x^n の計算
+    y_positive = x**n
+    y_negative = -x**n
 
     # y > 0 の領域をプロット
-    ax.plot(x, y_positive, zs=0, zdir='y', label='y = x^2 (y > 0)', color='b')
+    ax.plot(x, y_positive, zs=0, zdir='y', label=f'y = x^{n} (y > 0)', color='b')
 
     # y < 0 の領域をプロット
-    ax.plot(x, y_negative, zs=0, zdir='y', label='y = -x^2 (y < 0)', color='r')
+    ax.plot(x, y_negative, zs=0, zdir='y', label=f'y = -x^{n} (y < 0)', color='r')
 
     # グラフを90°傾ける
     ax.view_init(elev=20, azim=-90)
@@ -38,8 +39,9 @@ def plot_3d_graph():
 
 # アプリケーションを実行する部分
 if __name__ == '__main__':
-    plot_3d_graph()
-
+    # nの値をユーザーに入力させる
+    n = st.slider('nの値を選択してください', min_value=1, max_value=10, value=2)
+    plot_3d_graph(n)
 
 
 
